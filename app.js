407fe1522ca32d5c985d49b5f7f6785377ef6056
auditt98 <vietanh8i1998@gmail.com>
2020-02-23 20:47:52 +0700

    Configure for hosting

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'))
 const port = 3000 || process.env.PORT
 
-app.get('/', (req, res) => res.send('hello'))
-// app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+// app.get('', (req, res) => res.send('hello'))
+app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
