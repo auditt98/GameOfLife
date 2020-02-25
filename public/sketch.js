95c727af5d21599526589097461f205b9fd9ed00
auditt98 <vietanh8i1998@gmail.com>
2020-02-25 11:09:13 +0700

    Added various thing, fixed stuff too

--- public/sketch.js
+++ public/sketch.js
@@ -3,77 +3,84 @@ let sizeOfCell
 var spawnRate = 0 //Must be from 0 to 1, anything above 1 will make
 let game
 let grid
-let nCol = 35 // 10, 20, 40, 80, 100, 200, 400
+let nCol = 175 // 700
 let fps = 10
+let defaultFPS = fps
+let randomPopulatedColor = false
+let colorPopulated = "#000000"
+let colorUnpopulated = "#ffffff"
 
-let randomColor = false
-let fixedColor = {
-    r: 0,
-    g: 0,
-    b: 0
-}
 
 //Canvas size / numCol(or row) -> width of each col 
 
 function setup() {
     game = new Game()
-    frameRate(fps);
+    frameRate(fps)
+    document.getElementById('speedInput').value = fps
     // createCanvas(windowWidth, windowHeight)
     let canvas = createCanvas(700, 700)
     sizeOfCell = Math.floor(width/nCol)
     print(sizeOfCell)
+    document.getElementById('pauseButton').classList.add('button-error')
     // let canvas = createCanvas(1000, 1000)
     grid = new Grid()
     canvas.mousePressed(cMousePressed)
 }
 
 function draw() {
-    background(255);
+    background(255)
+    frameRate(fps)
     grid.draw()
 }
 
 function cMousePressed(){
     // print(mouseX + ', ' + mouseY)
     // calculate column and row of clicked position
-    var rect = canvas.getBoundingClientRect();
-    var trueX = Math.floor((mouseX + window.pageXOffset) / sizeOfCell);
-    var trueY = Math.floor((mouseY + window.pageYOffset + Math.floor(window.height - rect.bottom) + 8 ) / sizeOfCell);
-    // let clickedCol = floor(mouseX/width * 10) 
-    // let clickedRow = floor(mouseY/height * 10) 
+    // var rect = canvas.getBoundingClientRect();
+    var trueX = Math.floor(((mouseX + window.pageXOffset) / sizeOfCell));
+    var trueY = Math.floor(((mouseY + window.pageYOffset) / sizeOfCell));
+    // var trueY = Math.floor((mouseY + window.pageYOffset + Math.floor(window.height - rect.bottom) + 8 ) / sizeOfCell); 
     if(trueX <= nCol - 1 && trueY <= nCol - 1){
         grid.cells[trueX][trueY].state = 1
     }
 }
 
-function start(){
-    game.state = 1
-}
 
-function pause(){
-    game.state = 0
-}
-
-function reset(){
-    game.generation = 0
-    for(let col = 0; col < nCol; col++){
-        for(let row = 0; row < nCol; row++){
-            grid.cells[col][row].state = false
-        }
+function changeSpeed() {
+    let newFPS = Number(document.getElementById('speedInput').value)
+    if(newFPS > 0){
+        fps = newFPS
+    } else{
+        document.getElementById('speedInput').value = fps
     }
-    pause()
 }
 
-function cClicked() {
-    // var rect = canvas.getBoundingClientRect();
-    // var left = Math.floor(rect.left + window.pageXOffset);
-    // var top = Math.floor(rect.top + window.pageYOffset);
-    // var cellSize = canvas.cellSize;
-    // var clickEvent = {};
-    // clickEvent.cellX = Math.floor((evt.clientX - left + window.pageXOffset) / cellSize);
-    // clickEvent.cellY = Math.floor((evt.clientY - top + window.pageYOffset - 5) / cellSize); // TODO: Where's offset coming from?
-    // fn(clickEvent);
-
+function resetSpeed() {
+    fps = defaultFPS
+    document.getElementById('speedInput').value = defaultFPS
 }
+
+function updatePopulatedColor(){
+    let colorPickerPopulated = document.getElementById('populatedColor')
+    colorPopulated ='#' + colorPickerPopulated.innerHTML
+    colorPickerPopulated.style.backgroundColor = '#' + jscolor
+}
+
+function updateUnpopulatedColor(){
+    let colorPickerUnpopulated = document.getElementById('unpopulatedColor')
+    colorPickerUnpopulated.style.backgroundColor = '#' + jscolor
+    colorUnpopulated = '#' + colorPickerUnpopulated.innerHTML
+}
+
+function updateRandomColor(){
+    let randomColorCheckbox = document.getElementById('isRandomColor')
+    if(randomColorCheckbox.checked == true){
+        randomPopulatedColor = true
+    } else{
+        randomPopulatedColor = false
+    }
+}
+
 
 class Game{
     constructor(){
@@ -85,7 +92,39 @@ class Game{
         if(this.state == 1){
             this.generation++
         }
-        document.getElementById('gen').innerText = 'Generations: ' + this.generation
+        document.getElementById('gen').innerText = 'Thế hệ: ' + this.generation
+    }
+
+    setGameState(state){
+        this.state = state
+        let startButton = document.getElementById('startButton')
+        let pauseButton = document.getElementById('pauseButton')
+        if(this.state == 0){
+            if(startButton.classList.contains('button-success')){
+                startButton.classList.remove('button-success')
+            }
+            if(!pauseButton.classList.contains('button-error')){
+                pauseButton.classList.add('button-error')
+            }
+        }
+        if(this.state == 1){
+            if(!startButton.classList.contains('button-success')){
+                startButton.classList.add('button-success')
+            }
+            if(pauseButton.classList.contains('button-error')){
+                pauseButton.classList.remove('button-error')
+            }
+        }
+    }
+
+    resetGameState(){
+        this.generation = 0
+        for(let col = 0; col < nCol; col++){
+            for(let row = 0; row < nCol; row++){
+                grid.cells[col][row].state = false
+            }
+        }
+        this.setGameState(0)
     }
 }
 
@@ -145,23 +184,7 @@ class Cell{
         this.col = col,
         this.state = false,
         this.gen = 0,
-        this.neighbourCount = 0,
-        this.color = this.setColor()
-        
-    }
-
-    setColor(){
-        //get a random color for a cell
-        if(randomColor == true){
-            return {
-                r: Math.floor(Math.random() * 255),
-                g: Math.floor(Math.random() * 255),
-                b: Math.floor(Math.random() * 255)
-            }
-        } else{
-            return fixedColor
-        }
-
+        this.neighbourCount = 0
     }
 
     randomCellState(){
@@ -213,18 +236,20 @@ class Cell{
         // performance is super, super bad, can't handle grid larger than 100x100 well
         // this.getNeighbours()
         if(!this.state){
-            noFill()
+            fill(colorUnpopulated)
             stroke(1)
             strokeWeight(1)
         } else{
             noStroke()
-            fill(this.color.r, this.color.g, this.color.b)
+            if(randomPopulatedColor){
+                fill('#' +
+                Math.floor(Math.random() * 255).toString(16) + 
+                Math.floor(Math.random() * 255).toString(16) + 
+                Math.floor(Math.random() * 255).toString(16))
+            } else{
+                fill(colorPopulated)
+            }
         }
-        if(game.state == 1){
-            // this.updateCellState()
-        }
-       
-
         rect(this.col * sizeOfCell, this.row * sizeOfCell, sizeOfCell, sizeOfCell)
     }
 
